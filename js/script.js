119625616910b8fed32b3a84dc32ace4f77857dc
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-01-19 00:18:47 +0500

    chenged

---
+++ js/script.js
@@ -0,0 +1,79 @@
+//1-misol
+// 4 xonali sonning juft raqamlari sonini topuvchi algoritm yozing.
+// let num = +prompt(`ixtiyoriy 4 honali son kiritng`)
+// let res = `juft sonlar yoq`;
+// let counter = 0;
+// let b = num % 10;
+// let o = (num % 100 - b) / 10;
+// let y = (num - num % 100) / 100;
+// let m = (num % 1000 - y) / 1000;
+// if (b % 2 == 0) {
+//     res = `juft son`;
+//     counter++;
+//  }
+// if (o % 2 == 0) {
+//     res = `juft son`;
+//     counter++;
+//  }
+// if (y % 2 == 0) {
+//     res = `juft son`;
+//     counter++;
+//  }
+//  if (m % 2 == 0) {
+//     res = `juft son`;
+//     counter++;
+//  }
+//  console.log(counter)
+
+
+//2-misol
+//Agar berilgan son 3 xonali sonning raqamlari yeg'indisi juft hamda 5ga karali bolsa true aks holda folse qiymat qaytartuvchi algoritm yozing
+// let num = +prompt(`ixtiyoriy 3 honali sonn kiriting`)
+// let res = ``;
+// let sum = `folse`;
+// let b = num % 10;
+// let o = (num % 100 - b) / 10;
+// let y = (num - num % 100) / 100;
+// res = y + o + b ;
+// if (res % 2 == 0) {
+//     sum = `true`
+// }
+// console.log(sum)
+
+
+//3-misol
+//3 ta sonn berilgan. ulard orasidan ortachasini topuvchi algoritm yozing
+// let num1 = +prompt(`1-sonni kiriting`) a > b && a < c
+// let num2 = +prompt(`2-sonni kiriting`) b > a && b < c
+// let num3 = +prompt(`3-sonni kiriting`) c > b && c < b
+
+
+//4-misol
+// foydalanuvchi kopi bilan 4 xonali son kirita oladi (1 xonali 2 xonali yoki 3 xonali bolishi ham mumkin). ushbu kiritilgan sonning palidrom yoki  palidrom emasligini tekshiruvchi algoritm yozing
+// let num = +prompt(`ixtiyoriy 1, 2 , 3 , kopibilan 4 honali sonn kiritng`);
+// let res = `palidrom son emas`;
+// let b = num % 10;
+// let O = (num % 100 - b) / 10;
+// let y = (num - num % 100) / 100;
+// let m = (num % 1000 - y) / 1000;
+
+
+// if (b == O) {
+//     res = `palidrom son`;
+// }
+// if (b == y) {
+//     res = `palidrom son`;
+// }
+// if (b == m) {
+//     if (O == y) {
+//         res = `palidrom son`;
+//     }
+// }
+// console.log(res)
+
+
+
+
+
+
+
